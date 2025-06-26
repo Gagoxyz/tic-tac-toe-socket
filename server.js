74735bc3cc0f0a041bdaef73bcc704ade33dbd82
gagoxyz <dgagoc@gmail.com>
2025-06-26 21:16:00 +0200

    se añade dependencia dotenv para gestión puerto del servidor

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
 
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
@@ -113,6 +116,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Servidor escuchando en http://localhost:3000');
+server.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
