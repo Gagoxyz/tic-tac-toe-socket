4210a1857427fcf6ee7925971ba02914191a8ebd
gagoxyz <dgagoc@gmail.com>
2025-06-30 14:35:22 +0200

    add get para reenviar cualquier ruta que no sea un archivo a index.html

--- server.js
+++ server.js
@@ -116,6 +116,10 @@ io.on('connection', (socket) => {
     });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 server.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 });
